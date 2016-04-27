6f12fafcc99594854b2b6db1683d6c7f11c6e63f
ChristopherBoo <christopherboo66@gmail.com>
2016-04-27 19:49:31 +0800

    Create MoAlg.cpp

---
+++ MoAlg.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int SQRT = 350;
+
+struct Query {
+    int l, r, id;
+
+    Query (){}
+    Query (int _l, int _r, int _id) : l(_l), r(_r), id(_id) {}
+
+    bool operator < (const Query& b) const {
+        return (l / SQRT == b.l / SQRT ? r > b.r : l / SQRT < b.l / SQRT);
+    }
+};
+
+int main() {
+
+    return 0;
+}
