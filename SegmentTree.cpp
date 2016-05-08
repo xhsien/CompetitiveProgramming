b234b640ecdfd3150e5eae78a05cf6a6db7780c4
ChristopherBoo <christopherboo66@gmail.com>
2016-05-08 12:13:15 +0800

    Add recursive implementation

--- SegmentTree.cpp
+++ SegmentTree.cpp
@@ -92,6 +92,47 @@ public:
     }
 };
 
+/* --- Recursive Implementation --- */
+
+int N;
+vector<int> v, t;
+
+int combine(int i, int j) {
+    return i + j;
+}
+
+void build(int id = 1, int l = 0, int r = N) {
+    if (r - l < 2) {
+        t[id] = v[l];
+        return;
+    }
+
+    int mid = (l + r) / 2;
+    build(id<<1  , l, mid);
+    build(id<<1|1, mid, r);
+    t[id] = combine(t[id<<1], t[id<<1|1]);
+}
+
+void modify(int p, int val, int id = 1, int l = 0, int r = N) {     // set v[p] = val
+    if (r - l < 2) {
+        t[id] = val;
+        return;
+    }
+
+    int mid = (l + r) / 2;
+    build(id<<1  , l, mid);
+    build(id<<1|1, mid, r);
+    t[id] = combine(t[id<<1], t[id<<1|1]);
+}
+
+int query(int x, int y, int id = 1, int l = 0, int r = N) {         // query in range [l,r)
+    if (x >= r || l >= y) return 0;
+    if (x <= l && r <= y) return t[id];
+
+    int mid = (l + r) / 2;
+    return combine(query(x,y,id<<1,l,mid), query(x,y,id<<1|1,mid,r));
+}
+
 int main() {
 
     return 0;
