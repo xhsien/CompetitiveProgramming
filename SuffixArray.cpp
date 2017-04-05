3c8a5d44d105b9305b5dc4728f3657edc881d7a0
Boo Kai Hsien <christopherboo66@gmail.com>
2017-04-05 08:38:56 +0800

    Update SuffixArray.cpp

--- SuffixArray.cpp
+++ SuffixArray.cpp
@@ -1,63 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int _MAXN = 100000;
-
-/*
- * t : string input
- * n : length of string t
- * 
- * t must be appended with '$' or ' '
- */
-char T[_MAXN]; int _N;
-
-/*
- * ra : rank array
- * sa : suffix array
- */
-int RA[_MAXN], SA[_MAXN];
-
-/*
- * helper arrays
- */
-int tempRA[_MAXN], tempSA[_MAXN],  c[_MAXN];
-
-bool cmp(int i, int j) { 
-	return strcmp(T + i, T + j) < 0; 
-}
-
-void countingSort(int k) {
-  int maxChar = max(300, _N);
-  memset(c, 0, sizeof c);
-  for (int i = 0; i < _N; i++)
-  	c[i + k < _N ? RA[i + k] : 0]++;
+void countingSort(int len, int ra[], int sa[], vector<int>& tsa, int k) {
+  vector<int> c; c.assign(len, 0);
+  int maxChar = max(300, len);
+  for (int i = 0; i < len; i++)
+    c[i + k < len ? ra[i + k] : 0]++;
   for (int i = 0, sum = 0; i < maxChar; i++)
     sum += c[i], c[i] = sum - c[i];
-  for (int i = 0; i < _N; i++)
-    tempSA[c[SA[i]+k < _N ? RA[SA[i]+k] : 0]++] = SA[i];
-  for (int i = 0; i < _N; i++)
-    SA[i] = tempSA[i];
+  for (int i = 0; i < len; i++)
+    tsa[c[sa[i] + k < len ? ra[sa[i] + k] : 0]++] = sa[i];
+  for (int i = 0; i < len; i++)
+    sa[i] = tsa[i];
 }
 
-void constructSA() {
-  for (int i = 0; i < _N; i++)
-  	RA[i] = T[i];
-  for (int i = 0; i < _N; i++)
-  	SA[i] = i;
-  for (int k = 1, r; k < _N; k <<= 1) {
-    countingSort(k);
-    countingSort(0);
-    tempRA[SA[0]] = r = 0;
-    for (int i = 1; i < _N; i++)
-      tempRA[SA[i]] = (RA[SA[i]] == RA[SA[i-1]] && RA[SA[i]+k] == RA[SA[i-1]+k]) ? r : ++r;
-    for (int i = 0; i < _N; i++)
-      RA[i] = tempRA[i];
-    if (RA[SA[_N-1]] == _N - 1)
-    	break;
+void SuffixArray(char str[], int len, int ra[], int sa[]) {
+  vector<int> tra; tra.assign(len, 0);
+  vector<int> tsa; tsa.assign(len, 0);
+  for (int i = 0; i < len; i++)
+    ra[i] = str[i], sa[i] = i;
+  for (int k = 1, r; k < len; k <<= 1) {
+    countingSort(len, ra, sa, tsa, k);
+    countingSort(len, ra, sa, tsa, 0);
+    tra[sa[0]] = r = 0;
+    for (int i = 1; i < len; i++)
+      tra[sa[i]] = (ra[sa[i]] == ra[sa[i-1]] && ra[sa[i] + k] == ra[sa[i-1] + k]) ? r : ++r;
+    for (int i = 0; i < len; i++)
+      ra[i] = tra[i];
+    if (ra[sa[len - 1]] == len - 1)
+      break;
   }
 }
 
 int main() {
 
-	return 0;
+  return 0;
 }
